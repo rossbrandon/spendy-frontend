3b5c767df9f4f1071df97035519833386d2dd4e7
Ross Brandon <rosstafarian1@gmail.com>
2020-12-24 01:21:57 -0600

    Add Auth0 base

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import {Auth0Provider} from './contexts/auth0-context'
+import {BrowserRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Auth0Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Auth0Provider>,
     document.getElementById('root'),
 )
 
